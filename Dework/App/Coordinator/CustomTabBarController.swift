67bc15c3176264451c24d41d4aee0cad203de88e
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-09-15 15:02:34 -0300

    [Adjust] Layout at custom tabbaR

---
+++ Dework/App/Coordinator/CustomTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomTabBarController.swift
+//  Dework
+//
+//  Created by Vinicius Mangueira on 15/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import Foundation
